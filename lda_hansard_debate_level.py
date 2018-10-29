1b2fbe814d35daa4873e26df05cb61ffd0b8cd15
GavinAbercrombie <36740883+GavinAbercrombie@users.noreply.github.com>
2018-10-29 14:06:00 +0000

    Update lda_hansard_debate_level.py

--- lda_hansard_debate_level.py
+++ lda_hansard_debate_level.py
@@ -5,7 +5,6 @@ import numpy as np
 import textacy
 import spacy
 import pandas as pd
-#from string import digits
 
 csv.field_size_limit(sys.maxsize)
 
@@ -14,12 +13,10 @@ nlp = spacy.load('en')
 debates = []
 data = csv.reader(open('../debate_csvs/HanDeSeT.csv', 'r'))
 for row in data:
-	#debates.append([row[0], row[1]])
-	# adapted for handeset
+	# adapted for handeset which features 7 columns of text per document (row)
 	debates.append([row[1], row[6] + row[7] + row[8] + row[9] +row[10] +row[11]])
 
 df = pd.DataFrame(debates, columns=['title', 'text'])
-#print(debates)
 chat_concat = (df.sort_values('title').groupby('title')['text'].agg(lambda col: '\n'.join(col.astype(str))))
 docs = list(chat_concat.apply(lambda x: nlp(x)))
 corpus = textacy.corpus.Corpus(nlp, docs=docs)
@@ -32,5 +29,3 @@ doc_topic_matrix = model.transform(doc_term_matrix)
 
 for topic_idx, top_terms in model.top_topic_terms(vectorizer.id_to_term, top_n=10):
 	print('topic', topic_idx, ':', '   '.join(top_terms))
-
-print(doc_topic_matrix.shape)
